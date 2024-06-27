2ffc4ca05ee3bd3154ff15fe15e69f52f9dadd4b
Tu Nombre <jonjairba@gmail.com>
2024-06-27 01:57:31 -0600

    add env

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
-export const PORT = process.env.PORT || 7511; 
+export const PORT = process.env.PORT || 4000; 
 
-export const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://jonathanbj:JNXYassmv636LrfM@baulmuseo.7w93rph.mongodb.net/baul";  //"mongodb://localhost/baul"; //aqui se puede colocar link de mongoAtlas
+export const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/baul"; //"mongodb+srv://jonathanbj:JNXYassmv636LrfM@baulmuseo.7w93rph.mongodb.net/baul";   //aqui se puede colocar link de mongoAtlas
 
 export const TOKEN_SECRET = process.env.TOKEN_SECRET || "secret";
 
